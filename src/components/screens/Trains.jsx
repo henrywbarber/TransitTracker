5fd4e22f3919551c6a9fb093782f6180d498a510
chughes25 <connorhu12@gmail.com>
2025-01-23 22:06:39 -0600

    api key working

--- src/components/screens/Trains.jsx
+++ src/components/screens/Trains.jsx
@@ -82,12 +82,11 @@ function Trains() {
     }, []);
 
     const [predictions, setPredictions] = useState({});
-    
 
     const fetchPredictions = async (stopId) => {
         try {
             const response = await axios.get(
-                `https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${process.env.CTA_API_KEY}&stpid=${stopId}&outputType=JSON`
+                `https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${process.env.EXPO_PUBLIC_CTA_API_KEY}&stpid=${stopId}&outputType=JSON`
             );
             const arrivals = response.data.ctatt.eta || [];
             arrivals.sort((a, b) => new Date(a.arrT) - new Date(b.arrT)); // Sort by arrival time
